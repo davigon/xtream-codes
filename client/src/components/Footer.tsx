ac86569e765b1c5cf2f7766b36399ebaa0d778ec
davigon <davigon.personal@outlook.es>
2022-12-29 16:45:19 +0100

    feat(client): footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,19 @@
 import React from "react"
+import { Center, Text } from "@chakra-ui/react"
 
 export const Footer = () => {
   return (
-    <footer style={{ textAlign: "center" }}>
-      {new Date().getFullYear()}{" "}
-      <a href={"https://github.com/davigon"}>davigon</a>
-    </footer>
+    <Center py={4} h={"4rem"}>
+      <Text fontWeight="semibold">
+        {new Date().getFullYear() + "."} Proyecto de{" "}
+        <a
+          href={"https://github.com/davigon"}
+          target={"_blank"}
+          rel={"noopener noreferrer"}
+        >
+          davigon
+        </a>
+      </Text>
+    </Center>
   )
 }
