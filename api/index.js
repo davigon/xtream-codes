33308e953c95d1e850229cf6bb9d91289bb8dca0
davigon <davigon.personal@outlook.es>
2023-11-16 20:02:46 +0100

    chore(api): remove deta

--- api/index.js
+++ api/index.js
@@ -1,15 +1,12 @@
 const express = require("express")
 const app = express()
-const routes = require(process.env.DETA_RUNTIME
+const port = process.env.ENVIRONMENT ? process.env.ENVIRONMENT : 4000
+const routes = require(process.env.ENVIRONMENT
   ? "./dist/routes"
   : "./src/routes").default
 
 app.use("/api", routes)
 
-if (process.env.DETA_RUNTIME) module.exports = app
-else {
-  const port = 4000
-  app.listen(port, () => {
-    console.log(`Micro is running locally on http://localhost:${port}`)
-  })
-}
+app.listen(port, () => {
+  console.log(`Running on port ${port}`)
+})
